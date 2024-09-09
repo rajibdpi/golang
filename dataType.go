1f9b9547a742daa4a795bd45fa48cb010d92c213
Rajiib Ahmed <rajibdpi@gmail.com>
2024-09-09 12:49:57 +0600

    DataTypes

--- dataType.go
+++ dataType.go
@@ -1,5 +1,18 @@
-pakage main
+package main
+
 import "fmt"
-func main(){
-	fmt.println("Hello Go")
+
+func main() {
+
+	i := 5
+	f := 5.0
+	s := "Name"
+	b := true
+	print(i, f, s, b)
+
+	//array
+	names := [...]string{"Rajib", "Kohinur", "Tasfiya", "Tahiya", "Wazia"}
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
+	}
 }
